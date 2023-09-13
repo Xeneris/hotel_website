d2329c26eec5c735c3cae7405c2ac06ec78ae1d4
Xeneris <mark.kakoczki0@gmail.com>
2023-09-13 12:55:50 +0200

    add basic welcome/landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+        fontFamily: {
+            'open-sans': ["open-sans", "open-sans"],
+            'domine': ["domine", "domine"]
+        },
+    },
+    plugins: [],
 }
 
